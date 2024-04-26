57fd03689711db38bb8d04ebbc27129818d81a54
marutoo ozmaky <marawanraafat999@gmal.com>
2024-04-26 23:13:10 +0200

    update style

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
 const addTask = document.querySelector("form");
 const tasks = document.querySelector(".under-progress");
-const fineshed = document.querySelector(".done")
+const fineshed = document.querySelector(".done");
+
 addTask.addEventListener("submit" , function(event) {
     event.preventDefault();
     const taskeValue = document.querySelector("input").value;
     tasks.innerHTML += ` <span class="list">${taskeValue}</span>`;
 })
+
+tasks.addEventListenerall("click",function name(params) {
+    
+})
+
+
+
+
+
+
 
 
