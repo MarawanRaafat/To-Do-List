ae8d7011628726482ec1d387f4c34da9dd872bcb
marutoo ozmaky <marawanraafat999@gmal.com>
2024-04-27 05:05:40 +0200

    update JS

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ const addTask = document.querySelector("form");
 const tasks = document.querySelector(".under-progress");
 const fineshed = document.querySelector(".done");
 
+
 addTask.addEventListener("submit" , function(event) {
     event.preventDefault();
     const taskeValue = document.querySelector("input").value;
     tasks.innerHTML += ` <span class="list">${taskeValue}</span>`;
 })
 
-tasks.addEventListenerall("click",function name(params) {
-    
+tasks.addEventListener("click" , function () {
+    const sameValue = document.querySelectorAll("tasks").value;
+    fineshed.innerHTML += `<span class="end">${sameValue}</span>`;
+    tasks.remove;
+})
+
+fineshed.addEventListener("click",function(){
+    const sameValue = document.querySelectorAll("fineshed").value;
+    tasks.innerHTML += ` <span class="list">${sameValue}</span>`;
+    fineshed.remove
 })
 
 
